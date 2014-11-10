0a7e7f69c458469c171e91cbb7392baf72a21d2d
maoqiao <leon_zmq520@hotmail.com>
2014-11-10 11:51:33 +0100

    start server

---
+++ bin/start.js
@@ -0,0 +1,6 @@
+var app = require('../lib/app.js');
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+app.listen(8000);
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
